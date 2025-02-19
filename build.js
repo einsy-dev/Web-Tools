02652295cc0cfa82a01b0f4e9d5e3022d9584599
Einsy <eins.yeager@gmail.com>
2025-02-20 00:22:37 +0500

    fix

--- build.js
+++ build.js
@@ -1,3 +1,12 @@
 import fs from "fs";
 
-fs.copyFileSync("package.json", "dist/package.json");
+const pack = new Object(JSON.parse(fs.readFileSync("package.json", "utf-8")));
+
+delete pack.scripts;
+
+fs.writeFileSync(
+	"dist/package.json",
+	JSON.stringify(pack, null, 2)
+)
+fs.copyFileSync("README.md", "dist/README.md");
+fs.copyFileSync("LICENSE", "dist/LICENSE");
